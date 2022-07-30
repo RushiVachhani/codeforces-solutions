2eec20c1476e83101c4fae634d76eb2297d4aa7f
Rushi Vachhani <vachhanirushi30@gmail.com>
2022-07-30 11:58:22 +0530

    Added more problems

---
+++ 1325A.cpp
@@ -0,0 +1,29 @@
+/*
+*   Author: Rushi Vachhani
+*   Platform: Codeforces
+*   Problem_ID: 1325A
+*   Language: C++
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >>n;
+    cout<<1<<" "<<n-1<<"\n";
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
