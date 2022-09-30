7db5b3881da1bfe4ca35b40bf3c32b23d829d4bc
Rushi Vachhani <vachhanirushi30@gmail.com>
2022-09-30 22:13:48 +0530

    Added solution to 1351A

---
+++ 1351A.cpp
@@ -0,0 +1,29 @@
+/*
+*   Author: Rushi Vachhani
+*   Platform: Codeforces
+*   Problem_ID: 1351A
+*   Language: C++
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    cout<<a+b<<"\n";
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
